35a748c9a58d964d0566413d49fb30aef4c98129
Joao Pedro <joao.fpsilva98@gmail.com>
2025-01-04 15:24:13 -0300

    feat: adding switch color theme

---
+++ app/_components/toggleButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { MdToggleOn, MdToggleOff } from "react-icons/md"; // Ícones de toggle
+
+export default function ToggleThemeButton() {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") as "light" | "dark";
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    ).matches;
+    const initialTheme = savedTheme || (prefersDark ? "dark" : "light");
+
+    setTheme(initialTheme);
+    document.documentElement.classList.add(initialTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.classList.replace(theme, newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="theme-toggle-button"
+      aria-label="Toggle Theme"
+    >
+      {theme === "light" ? (
+        <MdToggleOn size={36} color="var(--text-color)" />
+      ) : (
+        <MdToggleOff size={36} color="var(--text-color)" />
+      )}
+    </button>
+  );
+}
