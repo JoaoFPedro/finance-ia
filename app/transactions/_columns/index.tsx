67069c77bd5118be8922aa69b0f278827f50963d
Joao Pedro <joao.fpsilva98@gmail.com>
2025-01-12 11:32:11 -0300

    removing comments

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -8,9 +8,6 @@ import {
 } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-
 export const transactionsColumns: ColumnDef<Transactions>[] = [
   {
     accessorKey: "name",
