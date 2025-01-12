8c994f1b176247e337b9a7981e521000e49c3f8a
Joao Pedro <joao.fpsilva98@gmail.com>
2025-01-12 11:31:33 -0300

    feat: adding correct dePara columns

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { Transactions, TransactionsType } from "@prisma/client";
+import {
+  Transactions,
+  TransactionsCategory,
+  TransactionsPaymentMethod,
+  TransactionsType,
+} from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 
 // This type is used to define the shape of our data.
@@ -15,22 +20,72 @@ export const transactionsColumns: ColumnDef<Transactions>[] = [
     accessorKey: "type",
     header: "Tipo",
     cell: ({ row: { original: transaction } }) => {
-      if (transaction.type === TransactionsType.DEPOSIT) {
-        return "Depósito";
-      }
+      return transaction.type === TransactionsType.DEPOSIT
+        ? "Depósito"
+        : transaction.type === TransactionsType.EXPENSE
+          ? "Despesa"
+          : transaction.type === TransactionsType.INVESTMENT
+            ? "Investimento"
+            : "Tipo desconhecido";
     },
   },
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) => {
+      return transaction.category === TransactionsCategory.HOUSING
+        ? "Casa"
+        : transaction.category === TransactionsCategory.TRANSPORTATION
+          ? "Transporte"
+          : transaction.category === TransactionsCategory.FOOD
+            ? "Alimentação"
+            : transaction.category === TransactionsCategory.ENTERTAINMENT
+              ? "Entretenimento"
+              : transaction.category === TransactionsCategory.HEALTH
+                ? "Saúde"
+                : transaction.category === TransactionsCategory.UTILITY
+                  ? "Utilidade"
+                  : transaction.category === TransactionsCategory.SALARY
+                    ? "Salário"
+                    : transaction.category === TransactionsCategory.EDUCATION
+                      ? "Educação"
+                      : "Outros";
+    },
   },
   {
     accessorKey: "paymentMethod",
     header: "Metodo",
+    cell: ({ row: { original: transaction } }) => {
+      return transaction.paymentMethod === TransactionsPaymentMethod.CREDIT_CARD
+        ? "Cartão de crédito"
+        : transaction.paymentMethod === TransactionsPaymentMethod.DEBIT_CARD
+          ? "Cartão de débito"
+          : transaction.paymentMethod ===
+              TransactionsPaymentMethod.BANK_TRANSFER
+            ? "Transferência Bancaria"
+            : transaction.paymentMethod === TransactionsPaymentMethod.BANK_SLIP
+              ? "Boleto"
+              : transaction.paymentMethod === TransactionsPaymentMethod.CASH
+                ? "Dinheiro"
+                : transaction.paymentMethod === TransactionsPaymentMethod.PIX
+                  ? "Pix"
+                  : "Outros";
+    },
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) => {
+      const formattedDate = new Date(transaction.date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        },
+      );
+      return formattedDate;
+    },
   },
   {
     accessorKey: "amount",
