d8f836374fd45c37a242ff08513079d3c715fa70
Joao Pedro <joao.fpsilva98@gmail.com>
2025-01-18 18:01:40 -0300

    feat: initialize transaction componente

--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -1,12 +1,159 @@
+"use client";
 import { ArrowDownUpIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { z } from "zod";
+import {
+  TransactionsCategory,
+  TransactionsPaymentMethod,
+  TransactionsType,
+} from "@prisma/client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { TRANSACTION_TYPE_OPTIONS } from "../_constants/transactions";
+
+const formSchema = z.object({
+  username: z.string().trim().min(1, {
+    message: "O nome é obrigatório",
+  }),
+  amount: z.number({
+    message: "O valor é obrigatório",
+  }),
+  type: z.nativeEnum(TransactionsType, {
+    required_error: "O tipo é obrigatória",
+  }),
+  category: z.nativeEnum(TransactionsCategory, {
+    required_error: "A categoria é obrigatória",
+  }),
+  paymentMethod: z.nativeEnum(TransactionsPaymentMethod, {
+    required_error: "O método de pagamento é obrigatório",
+  }),
+  date: z.date({
+    required_error: "A data é obrigatória",
+  }),
+});
+const onSubmit = () => {};
 const AddTransactionButton = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      amount: 0,
+    },
+  });
   return (
-    <Button className="rounded-full text-sm">
-      Adicionar Transação
-      <ArrowDownUpIcon />
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="rounded-full text-sm">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Adicionar Transação</DialogTitle>
+          <DialogDescription>Insira as informações abaixo</DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Digite o nome da transação..."
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Valor</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Digite o valor da transação..."
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tipo</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione o tipo..." />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {TRANSACTION_TYPE_OPTIONS.map((options) => {
+                        return (
+                          <SelectItem key={options.value} value={options.value}>
+                            {options.label}
+                          </SelectItem>
+                        );
+                      })}
+                    </SelectContent>
+                  </Select>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="mt-4 flex justify-end">
+              <Button variant="outline">Cancelar</Button>
+              <Button>Adicionar</Button>
+            </div>
+          </form>
+        </Form>
+        <DialogFooter></DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
