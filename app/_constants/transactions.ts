7168ee68585468af407e239460554560f41c3570
Joao Pedro <joao.fpsilva98@gmail.com>
2025-01-18 19:20:22 -0300

    feat: adding transactions type

--- app/_constants/transactions.ts
+++ app/_constants/transactions.ts
@@ -1,4 +1,8 @@
-import { TransactionsType } from "@prisma/client";
+import {
+  TransactionsCategory,
+  TransactionsPaymentMethod,
+  TransactionsType,
+} from "@prisma/client";
 
 export const TRANSACTION_TYPE_OPTIONS = [
   {
@@ -12,5 +16,74 @@ export const TRANSACTION_TYPE_OPTIONS = [
   {
     value: TransactionsType.INVESTMENT,
     label: "Investimento",
+  },
+];
+
+export const TRANSACTIONS_CATEGORY_OPTIONS = [
+  {
+    value: TransactionsCategory.HOUSING,
+    label: "Moradia",
+  },
+  {
+    value: TransactionsCategory.TRANSPORTATION,
+    label: "Transporte",
+  },
+  {
+    value: TransactionsCategory.FOOD,
+    label: "Comida",
+  },
+  {
+    value: TransactionsCategory.ENTERTAINMENT,
+    label: "Entretenimento",
+  },
+  {
+    value: TransactionsCategory.HEALTH,
+    label: "Saúde",
+  },
+  {
+    value: TransactionsCategory.UTILITY,
+    label: "Proveito",
+  },
+  {
+    value: TransactionsCategory.SALARY,
+    label: "Salário",
+  },
+  {
+    value: TransactionsCategory.EDUCATION,
+    label: "Educação",
+  },
+  {
+    value: TransactionsCategory.OTHER,
+    label: "Outros",
+  },
+];
+export const TRANSACTIONS_PAYMENTMETHOD_OPTIONS = [
+  {
+    value: TransactionsPaymentMethod.CREDIT_CARD,
+    label: "Cartão de crédito",
+  },
+  {
+    value: TransactionsPaymentMethod.DEBIT_CARD,
+    label: "Cartão de débito",
+  },
+  {
+    value: TransactionsPaymentMethod.BANK_TRANSFER,
+    label: "Transferência bancária",
+  },
+  {
+    value: TransactionsPaymentMethod.BANK_SLIP,
+    label: "Boleto bancário",
+  },
+  {
+    value: TransactionsPaymentMethod.CASH,
+    label: "Dinheiro",
+  },
+  {
+    value: TransactionsPaymentMethod.PIX,
+    label: "Pix",
+  },
+  {
+    value: TransactionsPaymentMethod.OTHER,
+    label: "Outro",
   },
 ];
