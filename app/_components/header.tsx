7b594cac70da50441775ed9369a0eaf87070da23
Joao Pedro <joao.fpsilva98@gmail.com>
2025-01-20 19:20:14 -0300

    feat: adding toggle button to header but not showing

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { useRouter } from "next/navigation";
+// import ToggleThemeButton from "./toggleButton";
 
 const Header = () => {
   const router = useRouter();
@@ -15,24 +16,30 @@ const Header = () => {
     router.push("/");
   };
   return (
-    <div className="flex justify-between border-red-600 p-4">
-      <div className="flex items-center space-x-3">
-        <Image src="/logo.png" alt="Finance AI" height={36} width={36} />
-        <h1 className="text-center text-2xl font-bold">finance.ai</h1>
+    <>
+      <div className="flex justify-between p-4">
+        <div className="flex items-center space-x-3">
+          <Image src="/logo.png" alt="Finance AI" height={36} width={36} />
+          <h1 className="text-center text-2xl font-bold">finance.ai</h1>
+        </div>
+        <div className="">
+          <Button variant="ghost" onClick={goToHome}>
+            Dashboard
+          </Button>
+          <Button variant="ghost" onClick={goToTransaction}>
+            Transações
+          </Button>
+          <Button variant="ghost">Assinatura</Button>
+        </div>
+
+        <div className="flex flex-col">
+          <UserButton showName appearance={{ baseTheme: dark }} />
+          {/* <div className="">
+            <ToggleThemeButton />
+          </div> */}
+        </div>
       </div>
-      <div>
-        <Button variant="ghost" onClick={goToHome}>
-          Dashboard
-        </Button>
-        <Button variant="ghost" onClick={goToTransaction}>
-          Transações
-        </Button>
-        <Button variant="ghost">Assinatura</Button>
-      </div>
-      <div>
-        <UserButton showName appearance={{ baseTheme: dark }} />
-      </div>
-    </div>
+    </>
   );
 };
 
