33e04a081ca95d38239338a3f104d669150ac3df
Joao Pedro <joao.fpsilva98@gmail.com>
2025-02-01 17:11:04 -0300

    feat: adding nav bar and hover when acessing specific page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,9 +4,11 @@ import { UserButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import Link from "next/link";
 import ToggleThemeButton from "./toggleButton";
+import { usePathname } from "next/navigation";
 // import ToggleThemeButton from "./toggleButton";
 
 const Header = () => {
+  const pathName = usePathname();
   return (
     <>
       <nav className="flex justify-between p-4">
@@ -14,9 +16,39 @@ const Header = () => {
           <Image src="/logo.png" alt="Finance AI" height={36} width={36} />
           <h1 className="-ml-3 text-2xl font-bold">finance.ai</h1>
           <div className="space-x-4">
-            <Link href="/"> Dashboard</Link>
-            <Link href="/transactions"> Transações</Link>
-            <Link href="/subscriber"> Assinatura</Link>
+            <Link
+              href="/"
+              className={
+                pathName === "/"
+                  ? "font-bold text-primary"
+                  : "text-muted-foreground"
+              }
+            >
+              {" "}
+              Dashboard
+            </Link>
+            <Link
+              href="/transactions"
+              className={
+                pathName === "/transactions"
+                  ? "font-bold text-primary"
+                  : "text-muted-foreground"
+              }
+            >
+              {" "}
+              Transações
+            </Link>
+            <Link
+              href="/subscriber"
+              className={
+                pathName === "/subscriber"
+                  ? "font-bold text-primary"
+                  : "text-muted-foreground"
+              }
+            >
+              {" "}
+              Assinatura
+            </Link>
           </div>
         </div>
 
