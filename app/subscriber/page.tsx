5881e0cc71b0678c9a82edd44057c375564ce6a9
Joao Pedro <joao.fpsilva98@gmail.com>
2025-02-01 17:25:34 -0300

    feat: ensure only loged user can access the pages

--- app/subscriber/page.tsx
+++ app/subscriber/page.tsx
@@ -1,4 +1,11 @@
-const SubscriberPage = () => {
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const SubscriberPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/");
+  }
   return (
     <>
       <h1>Hello world</h1>
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,9 +2,19 @@ import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
 import { transactionsColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-  const transactions = await db.transactions.findMany({});
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/");
+  }
+  const transactions = await db.transactions.findMany({
+    where: {
+      id: userId,
+    },
+  });
   return (
     <div className="space-y-6 p-4">
       <div className="flex justify-between">
