7d494c1aafecda76ee547a8b1da4009a288cdbfd
Joao Pedro <joao.fpsilva98@gmail.com>
2025-02-02 18:59:49 -0300

    refactoring summary card component

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -5,33 +5,36 @@ import {
   WalletIcon,
 } from "lucide-react";
 import SummaryCard from "./summary-card";
+import { ReactNode } from "react";
+
+const summaryData: {
+  icon: ReactNode;
+  title: string;
+  amount: number;
+  size?: "small" | "large";
+}[] = [
+  { title: "Saldo", icon: <WalletIcon />, amount: 2700, size: "large" },
+  { title: "Investimento", icon: <PiggyBankIcon />, amount: 3500 },
+  {
+    title: "Receita",
+    icon: <TrendingUpIcon className="text-primary" />,
+    amount: 8000,
+  },
+  {
+    title: "Despesas",
+    icon: <TrendingDownIcon className="text-red-700" />,
+    amount: 2950,
+  },
+];
 
 const SummaryCards = () => {
   return (
     <div className="space-y-6">
-      <SummaryCard
-        title="Saldo"
-        icon={<WalletIcon />}
-        amount={2700}
-        size="large"
-      />
-
+      <SummaryCard {...summaryData[0]} />
       <div className="grid grid-cols-3 gap-6">
-        <SummaryCard
-          title="Investimento"
-          icon={<PiggyBankIcon />}
-          amount={3500}
-        />
-        <SummaryCard
-          title="Receita"
-          icon={<TrendingUpIcon className="text-primary" />}
-          amount={8000}
-        />
-        <SummaryCard
-          title="Despesas"
-          icon={<TrendingDownIcon className="text-red-700" />}
-          amount={2950}
-        />
+        {summaryData.slice(1).map((item, index) => (
+          <SummaryCard key={index} {...item} />
+        ))}
       </div>
     </div>
   );
