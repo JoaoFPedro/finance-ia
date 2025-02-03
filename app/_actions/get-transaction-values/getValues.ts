21b8a9c32ac2c4668d61592c750d4d211a204eff
Joao Pedro <joao.fpsilva98@gmail.com>
2025-02-03 20:47:53 -0300

    feat: adding getSpentTotal method to get total spent

--- app/_actions/get-transaction-values/getValues.ts
+++ app/_actions/get-transaction-values/getValues.ts
@@ -1,9 +1,8 @@
 "use server";
 
 import { db } from "@/app/_lib/prisma";
-// import { auth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { TransactionsType } from "@prisma/client";
-
 import { Decimal } from "@prisma/client/runtime/library";
 
 export const getInvestmentTotal = async () => {
@@ -22,6 +21,29 @@ export const getInvestmentTotal = async () => {
     return totalAmount.toNumber();
   } catch (error) {
     console.error("Error fetching investment transactions:", error);
+    return 0;
+  }
+};
+
+export const getSpentTotal = async () => {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+    const transactions = await db.transactions.findMany({
+      where: { type: TransactionsType.EXPENSE, userId: userId },
+      select: { amount: true },
+    });
+    const totalAmount = transactions.reduce(
+      (sum, transaction) => sum.plus(transaction.amount),
+      new Decimal(0),
+    );
+
+    console.log("Total EXPENSE amount:", transactions);
+    return totalAmount.toNumber();
+  } catch (error) {
+    console.log("Error:", error);
     return 0;
   }
 };
