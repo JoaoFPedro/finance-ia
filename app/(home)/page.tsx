0b01153c4184705ed8f44505b25005d741bbd0f3
Joao Pedro <joao.fpsilva98@gmail.com>
2025-02-05 20:37:24 -0300

    feat: get searching params to use the url

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,33 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import SummaryCard from "./_components/summary-cards";
 import TimeSelect from "../_components/time-select";
+import SummaryCards from "./_components/summary-cards";
+import { isMatch } from "date-fns";
 
-const Home = async () => {
+{
+  /* 
+  A informação de que o parâmetro vem da URL é definida pelo Next.js App Router, que passa os parâmetros de busca (searchParams) automaticamente para os Server Components que utilizam a convenção de nomeação [nome].tsx ou (grupo)/page.tsx.
+
+Como o searchParams é obtido?
+No Next.js, quando um usuário acessa uma página com uma query string (exemplo: ?month=02), esses valores são automaticamente incluídos em searchParams quando a página é renderizada. 
+  */
+}
+interface HomeProps {
+  searchParams: {
+    month: string;
+  };
+}
+const Home = async ({ searchParams: { month } }: HomeProps) => {
   const { userId } = await auth();
-
   if (!userId) {
     console.log("userid", userId);
     redirect("/login");
+  }
+  console.log("month***", month);
+  const monthIsInvalid = !month || !isMatch(month, "MM");
+
+  if (monthIsInvalid) {
+    redirect("?month=1");
   }
   return (
     <div className="p-6">
@@ -16,7 +35,7 @@ const Home = async () => {
         <h1 className="mb-6 text-2xl">Dashboard</h1>
         <TimeSelect />
       </div>
-      <SummaryCard />
+      <SummaryCards month={month} />
     </div>
   );
 };
