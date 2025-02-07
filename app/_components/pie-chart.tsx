a549018d4eb452d0f40ef007fe1d0d2bd973a63c
Joao Pedro <joao.fpsilva98@gmail.com>
2025-02-06 21:16:43 -0300

    feat: adding pie chart component

--- app/_components/pie-chart.tsx
+++ app/_components/pie-chart.tsx
@@ -1,34 +1,76 @@
 "use client";
 
-import { TrendingUp } from "lucide-react";
+import { PiggyBankIcon, TrendingDownIcon, TrendingUpIcon } from "lucide-react";
 import { Pie, PieChart } from "recharts";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "./ui/card";
+import { Card, CardContent, CardFooter } from "./ui/card";
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
 } from "./ui/chart";
+import { Alert, AlertDescription } from "./ui/alert";
+import { useEffect, useState } from "react";
+import {
+  getInvestmentTotal,
+  getSpentTotal,
+  getDepositTotal,
+  getTotalBalance,
+} from "../_actions/get-transaction-values/getValues";
 
-const PieChartBalance = () => {
+interface PieChartProps {
+  month: string;
+}
+
+const PieChartBalance = (month: PieChartProps) => {
+  const [investmentTotal, setInvestmentTotal] = useState<number | null>(null);
+  const [spentTotal, setSpentTotal] = useState<number | null>(null);
+  const [balanceTotal, setBalanceTotal] = useState<number | null>(null);
+  const [totalBalance, setTotalBalance] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchTotal = async () => {
+      try {
+        const totalInvestment = await getInvestmentTotal(month);
+        const totalSpent = await getSpentTotal(month);
+        const balance = await getDepositTotal(month);
+        const totalBalanceSum = await getTotalBalance(month);
+
+        setInvestmentTotal(totalInvestment);
+        setSpentTotal(totalSpent);
+        setBalanceTotal(balance);
+        setTotalBalance(totalBalanceSum);
+      } finally {
+      }
+    };
+    fetchTotal();
+  }, [month]);
+  let totalDepositPieChart = 0;
+  let totalBalancePieChart = 0;
+  let totalInvestmentPieChart = 0;
+  if (totalBalance && spentTotal) {
+    totalDepositPieChart = (spentTotal / totalBalance) * 100;
+    totalBalancePieChart = (balanceTotal! / (totalBalance ?? 0)) * 100;
+    totalInvestmentPieChart = (investmentTotal! / (totalBalance ?? 0)) * 100;
+  }
   const chartData = [
-    { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-    { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-    { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
-    { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-    { browser: "other", visitors: 90, fill: "var(--color-other)" },
+    {
+      browser: "Deposito",
+      visitors: (spentTotal! / (totalBalance ?? 0)) * 100,
+      fill: "red",
+    },
+    {
+      browser: "Investimento",
+      visitors: (investmentTotal! / (totalBalance ?? 0)) * 100,
+      fill: "white",
+    },
+    {
+      browser: "Receita",
+      visitors: (balanceTotal! / (totalBalance ?? 0)) * 100,
+      fill: "green",
+    },
   ];
   const chartConfig = {
-    visitors: {
-      label: "Visitors",
-    },
     chrome: {
       label: "Chrome",
       color: "hsl(var(--chart-1))",
@@ -41,51 +83,66 @@ const PieChartBalance = () => {
       label: "Firefox",
       color: "hsl(var(--chart-3))",
     },
-    edge: {
-      label: "Edge",
-      color: "hsl(var(--chart-4))",
-    },
-    other: {
-      label: "Other",
-      color: "hsl(var(--chart-5))",
-    },
   } satisfies ChartConfig;
+
   return (
-    <>
-      <Card className="flex flex-col">
-        <CardHeader className="items-center pb-0">
-          <CardTitle>Pie Chart - Donut</CardTitle>
-          <CardDescription>January - June 2024</CardDescription>
-        </CardHeader>
-        <CardContent className="flex-1 pb-0">
-          <ChartContainer
-            config={chartConfig}
-            className="mx-auto aspect-square max-h-[250px]"
-          >
-            <PieChart>
-              <ChartTooltip
-                cursor={false}
-                content={<ChartTooltipContent hideLabel />}
-              />
-              <Pie
-                data={chartData}
-                dataKey="visitors"
-                nameKey="browser"
-                innerRadius={60}
-              />
-            </PieChart>
-          </ChartContainer>
-        </CardContent>
-        <CardFooter className="flex-col gap-2 text-sm">
-          <div className="flex items-center gap-2 font-medium leading-none">
-            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-          </div>
-          <div className="leading-none text-muted-foreground">
-            Showing total visitors for the last 6 months
-          </div>
-        </CardFooter>
-      </Card>
-    </>
+    <Card className="w-1/9 mt-6 flex flex-col">
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="visitors"
+              nameKey="browser"
+              innerRadius={60}
+            />
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="mt-5 flex-col space-y-3">
+        <Alert>
+          <AlertDescription className="flex justify-between">
+            <div className="flex gap-2">
+              <TrendingUpIcon className="rounded-md bg-[#FFFFFF08] p-1 text-primary" />
+
+              <h1>Ganhos</h1>
+            </div>
+
+            <h1>{`${totalBalancePieChart.toFixed(1)}%`}</h1>
+          </AlertDescription>
+        </Alert>
+
+        <Alert>
+          <AlertDescription className="flex justify-between">
+            <div className="flex gap-2">
+              <TrendingDownIcon className="rounded-md bg-[#FFFFFF08] p-1 text-primary text-red-700" />
+
+              <h1>Gastos</h1>
+            </div>
+
+            <h1 className="font-bold">{`${totalDepositPieChart.toFixed(1)}%`}</h1>
+          </AlertDescription>
+        </Alert>
+
+        <Alert>
+          <AlertDescription className="flex justify-between">
+            <div className="flex w-full gap-2">
+              <PiggyBankIcon className="rounded-md bg-[#FFFFFF08] p-1" />
+
+              <h1>Investimentos</h1>
+              <h1 className="font-bold">{`${totalInvestmentPieChart.toFixed(1)}%`}</h1>
+            </div>
+          </AlertDescription>
+        </Alert>
+      </CardFooter>
+    </Card>
   );
 };
 
