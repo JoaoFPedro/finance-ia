42338efa47689f0cd327e90e131caa010fe21dfc
Joao Pedro <joao.fpsilva98@gmail.com>
2025-02-07 19:20:39 -0300

    feat: initialize expenses by category component

---
+++ app/_components/expenses-by-category.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Progress } from "./ui/progress";
+import {
+  //   getDepositTotal,
+  //   getInvestmentTotal,
+  //   getSpentTotal,
+  getTotalBalance,
+} from "../_actions/get-transaction-values/getValues";
+
+interface ExpensesByCategoryProps {
+  month: string;
+}
+
+const ExpensesByCategory = (month: ExpensesByCategoryProps) => {
+  //   const [investmentTotal, setInvestmentTotal] = useState<number | null>(null);
+  //   const [spentTotal, setSpentTotal] = useState<number | null>(null);
+  //   const [balanceTotal, setBalanceTotal] = useState<number | null>(null);
+  const [totalBalance, setTotalBalance] = useState<number | null>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchTotal = async () => {
+      setLoading(true);
+
+      try {
+        // const totalInvestment = await getInvestmentTotal(month);
+        // const totalSpent = await getSpentTotal(month);
+        // const balance = await getDepositTotal(month);
+        const totalBalanceSum = await getTotalBalance(month);
+        console.log("GET_TOTAL_BALANCE***", totalBalanceSum);
+
+        // setInvestmentTotal(totalInvestment);
+        // setSpentTotal(totalSpent);
+        // setBalanceTotal(balance);
+        setTotalBalance(totalBalanceSum);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchTotal();
+  }, [month]);
+
+  if (loading) {
+    return <div className="text-center">Carregando...</div>;
+  }
+
+  console.log("BALANCETOTAL***", totalBalance);
+  return (
+    <div>
+      <h1>Gastos por categoria:</h1>
+      <Progress value={50} className="w-[60%]" />
+    </div>
+  );
+};
+
+export default ExpensesByCategory;
