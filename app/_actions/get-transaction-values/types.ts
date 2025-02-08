d251a05c119f5da76dfe473cce323c3de2f16193
Joao Pedro <joao.fpsilva98@gmail.com>
2025-02-08 19:41:12 -0300

    feat: adding expensesByCategory component

---
+++ app/_actions/get-transaction-values/types.ts
@@ -0,0 +1,7 @@
+import { TransactionsCategory } from "@prisma/client";
+
+export interface TotalExpensePerCategory {
+  category: TransactionsCategory;
+  totalAmount: number;
+  percentageOfTotal: number;
+}
--- app/_components/expenses-by-category.tsx
+++ app/_components/expenses-by-category.tsx
@@ -1,55 +1,67 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { Progress } from "./ui/progress";
-import {
-  //   getDepositTotal,
-  //   getInvestmentTotal,
-  //   getSpentTotal,
-  getTotalBalance,
-} from "../_actions/get-transaction-values";
+
+import { ScrollArea } from "./ui/scroll-area";
+import { TotalExpensePerCategory } from "../_actions/get-transaction-values/types";
+import { CardContent, CardHeader, CardTitle } from "./ui/card";
 
 interface ExpensesByCategoryProps {
-  month: string;
+  expensesByCategory: TotalExpensePerCategory[];
 }
 
-const ExpensesByCategory = (month: ExpensesByCategoryProps) => {
+const ExpensesByCategory = ({
+  expensesByCategory,
+}: ExpensesByCategoryProps) => {
+  console.log("EXPENSES***", expensesByCategory);
   //   const [investmentTotal, setInvestmentTotal] = useState<number | null>(null);
   //   const [spentTotal, setSpentTotal] = useState<number | null>(null);
   //   const [balanceTotal, setBalanceTotal] = useState<number | null>(null);
-  const [totalBalance, setTotalBalance] = useState<number | null>(null);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const fetchTotal = async () => {
-      setLoading(true);
+  // const [totalBalance, setTotalBalance] = useState<number | null>(null);
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   const fetchTotal = async () => {
+  //     setLoading(true);
 
-      try {
-        // const totalInvestment = await getInvestmentTotal(month);
-        // const totalSpent = await getSpentTotal(month);
-        // const balance = await getDepositTotal(month);
-        const totalBalanceSum = await getTotalBalance(month);
-        console.log("GET_TOTAL_BALANCE***", totalBalanceSum);
+  //     try {
+  //       // const totalInvestment = await getInvestmentTotal(month);
+  //       // const totalSpent = await getSpentTotal(month);
+  //       // const balance = await getDepositTotal(month);
+  //       const totalBalanceSum = await getTotalBalance(month);
+  //       console.log("GET_TOTAL_BALANCE***", totalBalanceSum);
 
-        // setInvestmentTotal(totalInvestment);
-        // setSpentTotal(totalSpent);
-        // setBalanceTotal(balance);
-        setTotalBalance(totalBalance);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchTotal();
-  }, [month, totalBalance]);
+  //       // setInvestmentTotal(totalInvestment);
+  //       // setSpentTotal(totalSpent);
+  //       // setBalanceTotal(balance);
+  //       setTotalBalance(totalBalance);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  //   fetchTotal();
+  // }, [month, totalBalance]);
 
-  if (loading) {
-    return <div className="text-center">Carregando...</div>;
-  }
+  // if (loading) {
+  //   return <div className="text-center">Carregando...</div>;
+  // }
 
   return (
-    <div>
-      <h1>Gastos por categoria:</h1>
-      <Progress value={50} className="w-[60%]" />
-    </div>
+    <ScrollArea className="broder col-span-2 h-full rounded-md p-6">
+      <CardHeader>
+        <CardTitle className="font-bold">Gastos por categoria</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {expensesByCategory.map((category) => (
+          <div key={category.category} className="space-y-2">
+            <div className="flex w-full justify-between">
+              <p className="text-sm font-bold">{category.category}</p>
+              <p className="text-sm font-bold">{category.percentageOfTotal}</p>
+            </div>
+            <Progress value={category.percentageOfTotal} />
+          </div>
+        ))}
+      </CardContent>
+    </ScrollArea>
   );
 };
 
