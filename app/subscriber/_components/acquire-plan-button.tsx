21d9400d8691dce3c9b2fe13281f7e94f3186e99
Joao Pedro <joao.fpsilva98@gmail.com>
2025-02-17 20:39:48 -0300

    refac: changing label button and action

--- app/subscriber/_components/acquire-plan-button.tsx
+++ app/subscriber/_components/acquire-plan-button.tsx
@@ -2,11 +2,14 @@
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckOut } from "../_actions/create-stripe-checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 const AcquirePlanButton = () => {
+  const { user } = useUser();
+
   const handleAcquirePlan = async () => {
     const { sessionId } = await createStripeCheckOut();
-
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHED_KEY) {
       throw new Error("Stripe key not found!");
     }
@@ -16,13 +19,28 @@ const AcquirePlanButton = () => {
 
     await stripe?.redirectToCheckout({ sessionId });
   };
+  const userHasProPlan = user?.publicMetadata.subscriptionPlan === "Pro";
+  if (userHasProPlan) {
+    return (
+      <Button
+        className="w-full rounded-3xl border border-primary text-primary"
+        variant="link"
+      >
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+        >
+          Gerenciar Plano
+        </Link>
+      </Button>
+    );
+  }
   return (
     <Button
       className="w-full rounded-3xl border border-primary text-primary"
       variant="ghost"
       onClick={handleAcquirePlan}
     >
-      Adquirar plano Pro{" "}
+      Adquirir Plano
     </Button>
   );
 };
