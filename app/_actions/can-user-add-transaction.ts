a2b806ce3fb2b0f6ba38aaf550334ef6722f20b6
Joao Pedro <joao.fpsilva98@gmail.com>
2025-02-26 19:52:02 -0300

    refac: removing uncessary method

--- app/_actions/can-user-add-transaction.ts
+++ app/_actions/can-user-add-transaction.ts
@@ -1,4 +1,4 @@
-import { auth, clerkClient } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { currentMonthTransaction } from "./get-current-month-transaction";
 
 export const canUserAddTransaction = async () => {
@@ -6,10 +6,7 @@ export const canUserAddTransaction = async () => {
   if (!userId) {
     throw new Error("Unauthorized");
   }
-  const user = await (await clerkClient()).users.getUser(userId);
-  if (user.publicMetadata.subscriptionPlan === "pro") {
-    return true;
-  }
+
   const currentMonthTransactions = await currentMonthTransaction();
   if (currentMonthTransactions && currentMonthTransactions >= 10) {
     return false;
