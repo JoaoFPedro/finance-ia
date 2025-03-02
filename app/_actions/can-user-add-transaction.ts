823c715fb370fbbab53eb41d902228cd95858b16
Joao Pedro <joao.fpsilva98@gmail.com>
2025-03-02 17:40:31 -0300

    fix: changing button disabled logic

--- app/_actions/can-user-add-transaction.ts
+++ app/_actions/can-user-add-transaction.ts
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { currentMonthTransaction } from "./get-current-month-transaction";
 
 export const canUserAddTransaction = async () => {
@@ -6,9 +6,16 @@ export const canUserAddTransaction = async () => {
   if (!userId) {
     throw new Error("Unauthorized");
   }
+  const clerk = await clerkClient(); // Primeiro resolva clerkClient()
+  const user = await clerk.users.getUser(userId); // Agora você pode acessar users
+  const hasProPlan = user.publicMetadata?.subscriptionPlan === "pro";
 
   const currentMonthTransactions = await currentMonthTransaction();
-  if (currentMonthTransactions && currentMonthTransactions >= 10) {
+  if (
+    currentMonthTransactions &&
+    currentMonthTransactions >= 10 &&
+    !hasProPlan
+  ) {
     return false;
   }
   return true;
