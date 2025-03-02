ba72fb0f7ea397a9f87022eefb8be9d64d94aa3e
Joao Pedro <joao.fpsilva98@gmail.com>
2025-03-02 09:27:56 -0300

    fix: changing sucess url

--- app/subscriber/_actions/create-stripe-checkout/index.ts
+++ app/subscriber/_actions/create-stripe-checkout/index.ts
@@ -17,7 +17,7 @@ export const createStripeCheckOut = async () => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "subscription",
-    success_url: "http://localhost:3000",
+    success_url: "https://finance-ia-liard.vercel.app/?month=1",
     cancel_url: "http://localhost:3000",
     subscription_data: {
       metadata: {
