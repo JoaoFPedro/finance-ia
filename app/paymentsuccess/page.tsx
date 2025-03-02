798ca16977b14a8344b18d87abe765168f33b076
Joao Pedro <joao.fpsilva98@gmail.com>
2025-03-02 10:34:48 -0300

    feat: adding paymentsucess page

---
+++ app/paymentsuccess/page.tsx
@@ -0,0 +1,26 @@
+import { Card, CardContent, CardHeader } from "../_components/ui/card";
+
+const NoData = () => {
+  // Encontra o mês correspondente
+
+  return (
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-center space-y-6 p-2">
+        <Card className="w-full max-w-xl p-6 text-center">
+          <CardContent>
+            <CardHeader>
+              <h2 className="mt-4 text-xl font-bold">
+                Obrigado por comprar com a gente!{" "}
+              </h2>
+            </CardHeader>
+            <p className="mb-5 mt-2">
+              Seu plano PRO e tudo que ele oferece será ativado em minutos!{" "}
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default NoData;
